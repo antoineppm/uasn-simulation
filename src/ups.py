6b153684a63a9e0d7a7d22a9b6c22783e30be37d
Antoine Maréchal <marechal.an@gmail.com>
2017-06-13 12:14:23 +0100

    Finished the UPS algorithm

--- src/ups.py
+++ src/ups.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
-from SimEnvironment import SimEnvironment
+from SimEnvironment import SimEnvironment, distance
 from UWNode import UWNode
+from math import sqrt
+import numpy as np
 
 class AnchorNode(UWNode):
 	"""Node that knows its position and takes part in the beaconing sequence"""
@@ -54,7 +56,7 @@ class SensorNode(UWNode):
 		"""
 		name = "sensor" + str(nb)
 		UWNode.__init__(self, name)
-		self.beaconsReceived = [None, None, None, None]
+		self.beaconsReceived = []
 	
 	def tick(self, time):
 		"""Function called every tick, lets the node perform operations
@@ -62,24 +64,83 @@ class SensorNode(UWNode):
 		If the sensor has received all four beacons, calculates and log its position
 		Never transmits
 		"""
+		if len(self.beaconsReceived) == 4:
+			pos = self.multilaterate()
+			if pos is None:
+				print self.name + " could not find its position"
+			else:
+				x,y,z = pos
+				print self.name + " found position: " + str((x, y, z))
+			print "actual position: " + str(self.position)
+			self.beaconsReceived = []
 		return ""
+	
+	def multilaterate(self):
+		"""Solves the equations to calculate the position of the node"""
+		# get parameters from the data received
+		x0, y0, z0, t0, dt0 = self.beaconsReceived[0]
+		x1, y1, z1, t1, dt1 = self.beaconsReceived[1]
+		x2, y2, z2, t2, dt2 = self.beaconsReceived[2]
+		x3, y3, z3, t3, dt3 = self.beaconsReceived[3]
+		# distances between beacons
+		dist01 = distance((x0,y0,z0),(x1,y1,z1))
+		dist12 = distance((x1,y1,z1),(x2,y2,z2))
+		dist23 = distance((x2,y2,z2),(x3,y3,z3))
+		# k coefficients
+		k1 = dist01 + (dt1 + t0 - t1) * self.speedOfSound
+		k2 = dist01 + dist12 + (dt1 + dt2 + t0 - t2) * self.speedOfSound
+		k3 = dist01 + dist12 + dist23 + (dt1 + dt2 + dt3 + t0 - t3) * self.speedOfSound
+		# solving linear equations
+		M = np.array([ [x0-x1,  y0-y1,  z0-z1],
+		               [x0-x2,  y0-y2,  z0-z2],
+		               [x0-x3,  y0-y3,  z0-z3] ])
+		A = np.array([ -k1,
+		               -k2,
+		               -k3 ])
+		B = np.array([ (k1*k1 + x0*x0 + y0*y0 + z0*z0 - x1*x1 - y1*y1 - z1*z1) / 2,
+		               (k2*k2 + x0*x0 + y0*y0 + z0*z0 - x2*x2 - y2*y2 - z2*z2) / 2,
+		               (k3*k3 + x0*x0 + y0*y0 + z0*z0 - x3*x3 - y3*y3 - z3*z3) / 2 ])
+		Ax, Ay, Az = np.linalg.solve(M, A)
+		Bx, By, Bz = np.linalg.solve(M, B)
+		# solving quadtratic equation
+		alpha = Ax*Ax + Ay*Ay + Az*Az - 1
+		beta = 2 * (Ax*Bx + Ay*By + Az*Bz - x0*Ax - y0*Ay - z0*Az)
+		gamma = Bx*Bx + By*By + Bz*Bz - 2 * (x0*Bx + y0*By + z0*Bz) + x0*x0 + y0*y0 + z0*z0
+		delta = sqrt(beta*beta - 4*alpha*gamma)
+		w1 = (-beta - delta) / (2*alpha)
+		w2 = (-beta + delta) / (2*alpha)
+		# calculating the coordinates
+		w = 0
+		if w1 == w2:
+			w = w1
+		elif w2 < 0:
+			w = w1
+		elif w1 < 0:
+			w = w2
+		else:
+			return None		# no unique position can be determined
+		x = Ax*w + Bx
+		y = Ay*w + By
+		z = Az*w + Bz
+		return (x,y,z)
 	
 	def receive(self, time, message):
 		"""Function called when a message broadcast by another node arrives at the node
 		time        -- date of reception (s)
 		message     -- message received
 		"""
-		print self.name + " received beacon at time " + str(time) + ": " + message
+		# print self.name + " received beacon at time " + str(time) + ": " + message
 		priority, x, y, z, delay = [float(s) for s in message.split()]
-		self.beaconsReceived(priority) = (x, y, z, time, delay)
+		self.beaconsReceived.append((x, y, z, time, delay))
 
 sim = SimEnvironment((500,500,200))
 
 sim.addNode(AnchorNode(0, (0, 0, 0)))
 sim.addNode(AnchorNode(1, (0, 500, 0)))
 sim.addNode(AnchorNode(2, (500, 250, 0)))
-sim.addNode(AnchorNode(3, (250, 250, 200)))
+sim.addNode(AnchorNode(3, (250, 250, -200)))
+
+for i in xrange(20):
+	sim.addNode(SensorNode(i))
 
 sim.run(2)
-	
-	
