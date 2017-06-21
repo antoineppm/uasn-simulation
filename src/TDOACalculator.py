4100c5aa73884aa1232eb5c6852993ec11ba4a01
Antoine Maréchal <marechal.an@gmail.com>
2017-06-21 16:49:07 +0100

    Added a different 4-lateration function providing more thorough data

--- src/TDOACalculator.py
+++ src/TDOACalculator.py
@@ -3,6 +3,8 @@
 from math import sqrt
 import numpy as np
 
+from SimEnvironment import distance
+
 class TDOACalculator:
 	"""Utility class for calculating (4-laterating) positions from TDoA information"""
 	def __init__(self):
@@ -108,6 +110,161 @@ class TDOACalculator:
 		y = Ay*w + By
 		z = Az*w + Bz
 		return ("ok", x,y,z, error)
+	
+	def calculatePositionVerbose(self, velocity):
+		"""Calculates a position from the stored data
+		velocity    -- speed of transmission of the messages (m/s)
+		Returns a string ("ok" or error message) and four floats (calculated X,Y,Z coordinates plus error estimate if successful, 0 otherwise)
+		"""
+		# extracts the anchor positions
+		for anchor in self.anchorPositions:
+			if anchor is None:
+				return None
+		x0, y0, z0 = self.anchorPositions[0]
+		x1, y1, z1 = self.anchorPositions[1]
+		x2, y2, z2 = self.anchorPositions[2]
+		x3, y3, z3 = self.anchorPositions[3]
+		
+		## cakculate separate positions from each data point
+		dataP = 0
+		positions = []
+		
+		for beacon in self.dataArchive.values():
+			if sum([ 1 if x is None else 0 for x in beacon ]) > 0:
+				return None
+			dataP += 1
+			# k coefficients
+			t0, dt0 = beacon[0]
+			t1, dt1 = beacon[1]
+			t2, dt2 = beacon[2]
+			t3, dt3 = beacon[3]
+			k1 = velocity * (t0 - dt0 - t1 + dt1)
+			k2 = velocity * (t0 - dt0 - t2 + dt2)
+			k3 = velocity * (t0 - dt0 - t3 + dt3)
+			# solving linear equations
+			M = np.array([ [x0-x1,  y0-y1,  z0-z1],
+			               [x0-x2,  y0-y2,  z0-z2],
+			               [x0-x3,  y0-y3,  z0-z3] ])
+			A = np.array([ -k1,
+			               -k2,
+			               -k3 ])
+			B = np.array([ (k1*k1 + x0*x0 + y0*y0 + z0*z0 - x1*x1 - y1*y1 - z1*z1) / 2,
+			               (k2*k2 + x0*x0 + y0*y0 + z0*z0 - x2*x2 - y2*y2 - z2*z2) / 2,
+			               (k3*k3 + x0*x0 + y0*y0 + z0*z0 - x3*x3 - y3*y3 - z3*z3) / 2 ])
+			Ax, Ay, Az = np.linalg.solve(M, A)
+			Bx, By, Bz = np.linalg.solve(M, B)
+			# solving quadtratic equation
+			alpha = Ax*Ax + Ay*Ay + Az*Az - 1
+			beta = 2 * (Ax*Bx + Ay*By + Az*Bz - x0*Ax - y0*Ay - z0*Az)
+			gamma = Bx*Bx + By*By + Bz*Bz - 2 * (x0*Bx + y0*By + z0*Bz) + x0*x0 + y0*y0 + z0*z0
+			delta = beta*beta - 4*alpha*gamma
+			w = 0
+			if delta < 0:
+				return None
+			elif delta == 0:
+				w = -beta / (2*alpha)
+			else:
+				w1 = (-beta - sqrt(delta)) / (2*alpha)
+				w2 = (-beta + sqrt(delta)) / (2*alpha)
+				if w2 < 0:
+					w = w1
+				elif w1 < 0:
+					w = w2
+				else:
+					w1 = (-beta - sqrt(delta)) / (2*alpha)
+					w2 = (-beta + sqrt(delta)) / (2*alpha)
+					if w2 < 0:
+						w = w1
+					elif w1 < 0:
+						w = w2
+					else:
+						continue
+				# calculating the coordinates
+				x = Ax*w + Bx
+				y = Ay*w + By
+				z = Az*w + Bz
+				positions.append((x, y, z))
+			
+			if len(positions) == 0:
+				return None
+			# calculating the average position
+			n = len(positions)
+			x = sum([ xx for xx, yy, zz in positions ]) / n
+			y = sum([ yy for xx, yy, zz in positions ]) / n
+			z = sum([ zz for xx, yy, zz in positions ]) / n
+			posAvg = (x, y, z)
+			# calculating the error (standard deviation)
+			posError = sqrt(sum([ distance(posAvg, p)**2 for p in positions ]) / n)
+		
+		## calculate single position from average k coeffs
+		# k coefficients
+		k1List = []
+		k2List = []
+		k3List = []
+		for beacon in self.dataArchive.values():
+			if beacon[0] is None:
+				continue
+			t0, dt0 = beacon[0]
+			if beacon[1] is not None:
+				t1, dt1 = beacon[1]
+				k1List.append(t0 - dt0 - t1 + dt1)
+			if beacon[2] is not None:
+				t2, dt2 = beacon[2]
+				k2List.append(t0 - dt0 - t2 + dt2)
+			if beacon[3] is not None:
+				t3, dt3 = beacon[3]
+				k3List.append(t0 - dt0 - t3 + dt3)
+		if len(k1List) == 0 or len(k2List) == 0 or len(k3List) == 0:
+			return None
+		kNb = []
+		kAvg = []
+		kStDev = []
+		for kList in [k1List, k2List, k3List]:
+			n = len(kList)
+			m = sum(kList) / n
+			s = velocity * sqrt(sum([ (k-m)**2 for k in kList ]) / n)
+			kNb.append(n)
+			kAvg.append(m)
+			kStDev.append(s)
+		kError = sqrt(sum([ s**2 for s in kStDev ]))
+		k1 = velocity * kAvg[0]
+		k2 = velocity * kAvg[1]
+		k3 = velocity * kAvg[2]
+		# solving linear equations
+		M = np.array([ [x0-x1,  y0-y1,  z0-z1],
+		               [x0-x2,  y0-y2,  z0-z2],
+		               [x0-x3,  y0-y3,  z0-z3] ])
+		A = np.array([ -k1,
+		               -k2,
+		               -k3 ])
+		B = np.array([ (k1*k1 + x0*x0 + y0*y0 + z0*z0 - x1*x1 - y1*y1 - z1*z1) / 2,
+		               (k2*k2 + x0*x0 + y0*y0 + z0*z0 - x2*x2 - y2*y2 - z2*z2) / 2,
+		               (k3*k3 + x0*x0 + y0*y0 + z0*z0 - x3*x3 - y3*y3 - z3*z3) / 2 ])
+		Ax, Ay, Az = np.linalg.solve(M, A)
+		Bx, By, Bz = np.linalg.solve(M, B)
+		# solving quadtratic equation
+		alpha = Ax*Ax + Ay*Ay + Az*Az - 1
+		beta = 2 * (Ax*Bx + Ay*By + Az*Bz - x0*Ax - y0*Ay - z0*Az)
+		gamma = Bx*Bx + By*By + Bz*Bz - 2 * (x0*Bx + y0*By + z0*Bz) + x0*x0 + y0*y0 + z0*z0
+		delta = beta*beta - 4*alpha*gamma
+		w = 0
+		if delta < 0:
+			return None
+		elif delta == 0:
+			w = -beta / (2*alpha)
+		else:
+			w1 = (-beta - sqrt(delta)) / (2*alpha)
+			w2 = (-beta + sqrt(delta)) / (2*alpha)
+			if w2 < 0:
+				w = w1
+			elif w1 < 0:
+				w = w2
+			else:
+				return None
+		# calculating the coordinates
+		posEst = (Ax*w + Bx, Ay*w + By, Az*w + Bz)
+		
+		return (positions, posAvg, posError, kNb, kAvg, kStDev, kError, posEst)
 
 # test script
 calc = TDOACalculator()
