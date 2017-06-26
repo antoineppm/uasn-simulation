d399833cfd101ab88e7ecfc619f04cccc5525400
Antoine Maréchal <marechal.an@gmail.com>
2017-06-26 13:16:34 +0100

    Added new method to solve the equations while propagating errors, using the uncertainty module

--- src/TDOACalculator.py
+++ src/TDOACalculator.py
@@ -2,6 +2,8 @@
 
 from math import sqrt
 import numpy as np
+import uncertainties.umath as um
+import uncertainties.unumpy as unp
 
 from SimEnvironment import distance
 
@@ -85,6 +87,47 @@ class TDOACalculator:
 		z = Az*w + Bz
 		return ("ok", x,y,z)
 	
+	def fourLaterateWithError(self, K):
+		"""Does the core calculation from the anchor positions and the delay coefficients, including error calculations
+		K           - array of coefficients measuring the delays between anchor 0 and anchor 1, 2 and 3, including the uncertainty
+		Returns a string ("ok" or error message) and four floats (calculated X,Y,Z coordinates and error if successful, 0 otherwise)
+		"""
+		# extract the anchor positions
+		for anchor in self.anchorPositions:
+			if anchor is None:
+				return ("missing anchors", 0, 0, 0, 0)
+		P = [ np.array([x, y, z]) for x, y, z in self.anchorPositions ]
+		# solving linear equations
+		M = 2 * (P[0] - np.stack((P[1], P[2], P[3])))
+		I = -2 * K
+		J = K*K + P[0].dot(P[0]) - np.array([ P[i].dot(P[i]) for i in xrange(1,4) ])
+		A = np.linalg.inv(M).dot(I)
+		B = np.linalg.inv(M).dot(J)
+		# solving quadratic equation
+		alpha = A.dot(A) - 1
+		beta = 2*A.dot(B) - 2*A.dot(P[0])
+		gamma = B.dot(B) - 2*B.dot(P[0]) + P[0].dot(P[0])
+		delta = beta*beta - 4*alpha*gamma
+		w = 0
+		if delta < 0:
+			return ("no solution", 0, 0, 0, 0)
+		elif delta == 0:
+			w = -beta / (2*alpha)
+		else:
+			w1 = (-beta - um.sqrt(delta)) / (2*alpha)
+			w2 = (-beta + um.sqrt(delta)) / (2*alpha)
+			if w2 < 0:
+				w = w1
+			elif w1 < 0:
+				w = w2
+			else:
+				return ("two solutions", 0, 0, 0, 0)
+		# calculating the coordinates
+		P = A*w + B
+		x, y, z = unp.nominal_values(P)
+		e = np.linalg.norm(unp.std_devs(P))
+		return ("ok", x, y, z, e)
+	
 	def calculatePosition(self, velocity):
 		"""Calculates a position from the stored data
 		velocity    -- speed of transmission of the messages (m/s)
@@ -121,6 +164,41 @@ class TDOACalculator:
 		msg, x, y, z = self.fourLaterate(k1, k2, k3)
 		if msg == "ok":
 			return ("ok", x, y, z, error)
+		else:
+			return (msg, 0, 0, 0, 0)
+	
+	def calculatePositionWithError(self, velocity):
+		"""Calculates a position from the stored data
+		velocity    -- speed of transmission of the messages (m/s)
+		Returns a string ("ok" or error message) and four floats (calculated X,Y,Z coordinates plus error estimate if successful, 0 otherwise)
+		"""
+		kList = [ [], [], [] ]
+		for beacon in self.dataArchive.values():
+			if beacon[0] is None:
+				continue
+			t0, dt0 = beacon[0]
+			if beacon[1] is not None:
+				t1, dt1 = beacon[1]
+				kList[0].append(t0 - dt0 - t1 + dt1)
+			if beacon[2] is not None:
+				t2, dt2 = beacon[2]
+				kList[1].append(t0 - dt0 - t2 + dt2)
+			if beacon[3] is not None:
+				t3, dt3 = beacon[3]
+				kList[2].append(t0 - dt0 - t3 + dt3)
+		average = np.zeros((3))
+		stdev = np.zeros((3))
+		for i in xrange(3):
+			kl = kList[i]
+			length = len(kl)
+			if length == 0:
+				return ("not enough data", 0, 0, 0, 0)
+			average[i] = sum(kl) / length
+			stdev[i] = sqrt(sum([ (x - average[i])**2 for x in kl ]) / length)
+		K = velocity * unp.uarray(average, stdev)
+		msg, x, y, z, e = self.fourLaterateWithError(K)
+		if msg == "ok":
+			return ("ok", x, y, z, e)
 		else:
 			return (msg, 0, 0, 0, 0)
 	
