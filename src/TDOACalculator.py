c63c1ff294880b143b5e0b4ac756b177002e58ec
Antoine Maréchal <marechal.an@gmail.com>
2017-07-04 11:56:46 +0100

    Modified the regular TDOA calculation function to use the new error-aware solving function, removed the old solving function

--- src/TDOACalculator.py
+++ src/TDOACalculator.py
@@ -35,59 +35,7 @@ class TDOACalculator:
 			self.dataArchive[id] = [None, None, None, None]
 		self.dataArchive[id][anchor] = (toa, delay)
 	
-	def fourLaterate(self, k1, k2, k3):
-		"""Does the core calculation from the anchor positions and the delay coefficients
-		k1          -- coefficient measuring the delay between anchor 0 and anchor 1
-		k2          -- coefficient measuring the delay between anchor 0 and anchor 2
-		k3          -- coefficient measuring the delay between anchor 0 and anchor 3
-		Returns a string ("ok" or error message) and three floats (calculated X,Y,Z coordinates if successful, 0 otherwise)
-		"""
-		# extracts the anchor positions
-		for anchor in self.anchorPositions:
-			if anchor is None:
-				return ("missing anchors", 0, 0, 0)
-		x0, y0, z0 = self.anchorPositions[0]
-		x1, y1, z1 = self.anchorPositions[1]
-		x2, y2, z2 = self.anchorPositions[2]
-		x3, y3, z3 = self.anchorPositions[3]
-		# solving linear equations
-		M = np.array([ [x0-x1,  y0-y1,  z0-z1],
-		               [x0-x2,  y0-y2,  z0-z2],
-		               [x0-x3,  y0-y3,  z0-z3] ])
-		A = np.array([ -k1,
-		               -k2,
-		               -k3 ])
-		B = np.array([ (k1*k1 + x0*x0 + y0*y0 + z0*z0 - x1*x1 - y1*y1 - z1*z1) / 2,
-		               (k2*k2 + x0*x0 + y0*y0 + z0*z0 - x2*x2 - y2*y2 - z2*z2) / 2,
-		               (k3*k3 + x0*x0 + y0*y0 + z0*z0 - x3*x3 - y3*y3 - z3*z3) / 2 ])
-		Ax, Ay, Az = np.linalg.solve(M, A)
-		Bx, By, Bz = np.linalg.solve(M, B)
-		# solving quadtratic equation
-		alpha = Ax*Ax + Ay*Ay + Az*Az - 1
-		beta = 2 * (Ax*Bx + Ay*By + Az*Bz - x0*Ax - y0*Ay - z0*Az)
-		gamma = Bx*Bx + By*By + Bz*Bz - 2 * (x0*Bx + y0*By + z0*Bz) + x0*x0 + y0*y0 + z0*z0
-		delta = beta*beta - 4*alpha*gamma
-		w = 0
-		if delta < 0:
-			return ("no solution", 0, 0, 0)
-		elif delta == 0:
-			w = -beta / (2*alpha)
-		else:
-			w1 = (-beta - sqrt(delta)) / (2*alpha)
-			w2 = (-beta + sqrt(delta)) / (2*alpha)
-			if w2 < 0:
-				w = w1
-			elif w1 < 0:
-				w = w2
-			else:
-				return ("two solutions", 0, 0, 0)
-		# calculating the coordinates
-		x = Ax*w + Bx
-		y = Ay*w + By
-		z = Az*w + Bz
-		return ("ok", x,y,z)
-	
-	def fourLaterateWithError(self, K):
+	def fourLaterate(self, K):
 		"""Does the core calculation from the anchor positions and the delay coefficients, including error calculations
 		K           - array of coefficients measuring the delays between anchor 0 and anchor 1, 2 and 3, including the uncertainty
 		Returns a string ("ok" or error message) and four floats (calculated X,Y,Z coordinates and error if successful, 0 otherwise)
@@ -135,6 +83,7 @@ class TDOACalculator:
 		"""
 		# statistical measurements and k coefficients
 		kList = [ [], [], [] ]
+		
 		for beacon in self.dataArchive.values():
 			if beacon[0] is None:
 				continue
@@ -148,31 +97,24 @@ class TDOACalculator:
 			if beacon[3] is not None:
 				t3, dt3 = beacon[3]
 				kList[2].append(t0 - dt0 - t3 + dt3)
-		average = [0, 0, 0]
+		
+		avg = [0, 0, 0]
 		stdev = [0, 0, 0]
 		for i in xrange(3):
 			kl = kList[i]
-			length = len(kl)
-			if length == 0:
+			l = len(kl)
+			if l == 0:
 				return ("not enough data", 0, 0, 0, 0)
-			average[i] = sum(kl) / length
-			stdev[i] = sqrt(sum([ (x - average[i])**2 for x in kl ]) / length)
-		error = velocity * sqrt(sum([ s*s for s in stdev ]))
-		k1 = velocity * average[0]
-		k2 = velocity * average[1]
-		k3 = velocity * average[2]
-		msg, x, y, z = self.fourLaterate(k1, k2, k3)
-		if msg == "ok":
-			return ("ok", x, y, z, error)
-		else:
-			return (msg, 0, 0, 0, 0)
+			avg[i] = sum(kl) / l
+			stdev[i] = sqrt(sum([ (k-avg[i])**2 for k in kl ]) / l)
+		K = velocity * unp.uarray(avg, stdev)
+		return self.fourLaterate(K)
 	
 	def calculatePositionVerbose(self, velocity):
 		"""Calculates a position from the stored data
 		velocity    -- speed of transmission of the messages (m/s)
-		Returns a string ("ok" or error message) and four floats (calculated X,Y,Z coordinates plus error estimate if successful, 0 otherwise)
+		Returns a dictionary of various data, position estimates and error estimates
 		"""
-		## data to return
 		fullK = [ [], [], [] ]
 		cleanK = [ [], [], [] ]
 		
@@ -214,7 +156,7 @@ class TDOACalculator:
 			avg[i] = sum(kl) / l
 			stdev[i] = sqrt(sum([ (k-avg[i])**2 for k in kl ]) / l)
 		K = velocity * unp.uarray(avg, stdev)
-		msg, x, y, z, e = self.fourLaterateWithError(K)
+		msg, x, y, z, e = self.fourLaterate(K)
 		
 		fullKavg = avg
 		fullKsdv = stdev
@@ -232,7 +174,7 @@ class TDOACalculator:
 			avg[i] = sum(kl) / l
 			stdev[i] = sqrt(sum([ (k-avg[i])**2 for k in kl ]) / l)
 		K = velocity * unp.uarray(avg, stdev)
-		msg, x, y, z, e = self.fourLaterateWithError(K)
+		msg, x, y, z, e = self.fourLaterate(K)
 		
 		if msg != "ok":
 			return None
@@ -250,7 +192,7 @@ class TDOACalculator:
 		
 		for i in xrange(len(cleanK[0])):
 			K = velocity * np.array([ kl[i] for kl in cleanK ])
-			msg, x, y, z, e = self.fourLaterateWithError(K)
+			msg, x, y, z, e = self.fourLaterate(K)
 			if e > 1e-8:
 				print e
 			if msg == "ok":
