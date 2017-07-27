e372a5eadd9c9034e517ec1237067b6518c9de1e
Antoine Maréchal <marechal.an@gmail.com>
2017-07-27 14:11:37 +0100

    Modifications in test code

--- src/2DTDOA.py
+++ src/2DTDOA.py
@@ -39,13 +39,27 @@ def threeLaterate(Ak, K):
 		else:
 			return [A*w1 + B, A*w2 + B]
 
-def generateData(Ak, x, y, N=10, sigma=0.05):
+def speedOfSound(p1, p2, speedMatrix):
+	"""Interpolates the speed of sound at the middle of two points
+	p1          -- coordinates X,Y of the first point
+	p2          -- coordinates X,Y of the second point
+	speedMatrix -- speed of sound at each corner of the square (0,1)
+	"""
+	x, y = (p1 + p2) / 2
+	v = speedMatrix
+	v = np.average(v, axis=0, weights=(1-x, x))
+	v = np.average(v, axis=0, weights=(1-y, y))
+	return v
+	
+
+def generateData(Ak, x, y, N=10, speedMatrix=np.ones((2,2)), sigma=0.001):
 	"""Generates a set of data (range differences) for a particular point
 	Ak          -- np.array 3x2: coordinates X,Y of the three anchors
 	x           -- X coordinate of the point
 	y           -- Y coordinate of the point
 	N           -- number of data points to create
-	sigma       -- standard deviation of the ranging error
+	speedMatrix -- speed of sound at each corner of the square (0,1)
+	sigma       -- standard deviation of the additional ranging error
 	Returns:
 	- an array 1x2: real range differences
 	- an array Nx2: series of range dfferences between anchor 0 and anchor 1/2
@@ -57,9 +71,12 @@ def generateData(Ak, x, y, N=10, sigma=0.05):
 	range2 = np.linalg.norm(Ak[2] - P)
 	K = np.array([range0-range1, range0-range2])
 	for i in xrange(N):
-		r0 = range0 * gauss(1, sigma)
-		r1 = range1 * gauss(1, sigma)
-		r2 = range2 * gauss(1, sigma)
+		v0 = speedOfSound(P, Ak[0], speedMatrix)
+		r0 = range0 / v0 * gauss(1, sigma)
+		v1 = speedOfSound(P, Ak[1], speedMatrix)
+		r1 = range1 / v1 * gauss(1, sigma)
+		v2 = speedOfSound(P, Ak[2], speedMatrix)
+		r2 = range2 / v2 * gauss(1, sigma)
 		data.append([r0-r1, r0-r2])
 	return K, np.array(data)
 
@@ -74,7 +91,7 @@ if plot == "plane":
 		plt.scatter(ax, ay, c=(0,0,0,0), marker='o', s=100, lw=2, edgecolors='k')
 	plt.title("")
 
-step = 10
+step = 2 if plot == "plane" else 10
 nb = 3 * step + 1.
 
 xAxis = np.arange(nb)/step - 1
@@ -82,59 +99,18 @@ yAxis = xAxis
 
 nodes = np.swapaxes(np.meshgrid(xAxis, yAxis), 0, 2).reshape(-1, 2)     # numpy sorcery to obtain a list of 2D points on a grid
 
-realError = []
-estError1 = []
-estError2 = []
-estError3 = []
+xdata = [ [], [], [] ]
+ydata = [ [], [], [], [], [] ]
+
+vsigma = 0.0
+speeds = 1 + vsigma * np.random.randn(2,2)
 
 for x, y in nodes:
 	pos = np.array([x, y])
 	
-	sigma = 0.1
-	r = gauss(0, sigma)
-	a = uniform(0, pi)
-	realX = x + r * cos(a)
-	realY = y + r * sin(a)
-	realPos = np.array([realX, realY])
-	
-	realK, data = generateData(Ak, realX, realY, sigma=0.01)
+	realK, data = generateData(Ak, x, y, speedMatrix=speeds, sigma=0.02)
 	avg = sum(data) / len(data)
 	stdev = np.sqrt(sum((data-avg)**2) / len(data))
-	K = unp.uarray(avg, stdev)
-	
-	p = unp.nominal_values(threeLaterate(Ak, K))
-	
-	color = {0: 'k', 1:'b', 2:'r'}[len(p)]
-	
-	if len(p) == 2:
-		d0 = np.linalg.norm(p[0] - pos)
-		d1 = np.linalg.norm(p[1] - pos)
-		if d0 < d1:
-			p = [ p[0] ]
-			color = 'g'
-		elif d1 < d0:
-			p = [ p[1] ]
-			color = 'g'
-	
-	if plot == "plane":
-		plt.scatter(realX, realY, c=color, marker='s', lw=0)
-		
-	if len(p) == 1:
-		ex, ey = p[0]
-		if plot == "plane":
-			plt.scatter(ex, ey, c=color, marker='+')
-			plt.plot([realX,ex], [realY,ey], color+':')
-		if plot == "data":
-			e = np.linalg.norm(p[0] - pos)
-			if e <= 0.3:
-				realError.append(np.linalg.norm(p[0] - realPos))
-				estError1.append(e)
-		
-	
-	realK, data = generateData(Ak, x, y, sigma=0.02)
-	avg = sum(data) / len(data)
-	stdev = np.sqrt(sum((data-avg)**2) / len(data))
-	# stdev = 0.02 * np.ones(2)
 	K = unp.uarray(avg, stdev)
 
 	p = threeLaterate(Ak, K)
@@ -143,7 +119,8 @@ for x, y in nodes:
 		plt.scatter(x, y, c=color, marker='s', lw=0)
 	
 	if len(p) == 1:
-		ex, ey = unp.nominal_values(p)[0]
+		epos = unp.nominal_values(p)[0]
+		ex, ey = epos
 		if plot == "plane":
 			plt.scatter(ex, ey, c='r', marker='+')
 			plt.plot([x,ex], [y,ey], 'r:')
@@ -151,50 +128,88 @@ for x, y in nodes:
 		sigList = []
 		d = []
 		
-		N = 4
+		N = 8
 		for i in xrange(N):
 			a = 2 * pi * i / N
-			f = 0.5
+			f = 1
 			k = unp.uarray(avg + stdev*[cos(a),sin(a)]*f, np.zeros(2))
 			pp = threeLaterate(Ak, k)
-			if len(pp) == 1:
+			if len(pp) == 0:
+				continue
+			elif len(pp) == 1:
 				xx, yy = unp.nominal_values(pp)[0]
-				sigList.append([xx, yy])
-				d.append(sqrt( (ex-xx)**2 + (ey-yy)**2 ))
+			elif len(pp) == 2:
+				d0 = np.linalg.norm(unp.nominal_values(pp)[0] - epos)
+				d1 = np.linalg.norm(unp.nominal_values(pp)[1] - epos)
+				xx, yy = unp.nominal_values(pp)[ 0 if d0 < d1 else 1 ]
+			sigList.append([xx, yy])
+			d.append(sqrt( (ex-xx)**2 + (ey-yy)**2 ))
 		
 		if len(sigList) == N:
 			sigList = np.array(sigList)
-			realError.append(sqrt( (x-ex)**2 + (y-ey)**2 ))
-			estError1.append(sum(d) / len(d))
-			estError2.append(max(d))
-			nx, ny = sum(sigList) / N
-			estError3.append(sqrt( (nx-ex)**2 + (ny-ey)**2 ))
-			# nex, ney = np.sqrt(sum((coordList - [nx,ny])**2) / len(coordList))
-			# ne = np.linalg.norm([nex,ney])
+			npos = sum(sigList) / N
+			nx, ny = npos
+			xlist = sigList[:,0].tolist()
+			ylist = sigList[:,1].tolist()
+			xlist.append(xlist[0])
+			ylist.append(ylist[0])
+			
+			maxdim = max( [ max( [ np.linalg.norm(p1-p2) for p2 in sigList ] ) for p1 in sigList ] )
+			
+			area = 0
+			for i in xrange(N):
+				ax = xlist[i]
+				ay = ylist[i]
+				bx = xlist[i+1]
+				by = ylist[i+1]
+				area += abs( (ex*(ay-by) + ax*(by-ey) + bx*(ey-ay)) / 2 )
+			
 			if plot == "plane":
 				plt.scatter(nx, ny, c='k', marker='+')
 				plt.plot([x,nx], [y,ny], 'k:')
-				# plt.scatter(nx, ny, c=(0,0,0,0.2), marker='o', lw=0, s=100*ne)
-				xlist = sigList[:,0].tolist()
-				ylist = sigList[:,1].tolist()
-				xlist.append(xlist[0])
-				ylist.append(ylist[0])
 				plt.plot(xlist, ylist, 'k:')
+				print x, y
+				print ex, ey, np.linalg.norm(epos - pos)
+				print maxdim, area
+				print ""
+			if plot == "data":
+				apos = (2*epos + N*npos) / (2+N)
+				for i, p in enumerate([epos, npos, apos]):
+					xdata[i].append(np.linalg.norm(p - pos))
+					ydata[i].append( sqrt(sum([ np.linalg.norm(p-sigp)**2 for sigp in sigList ]) / N) )
+				ydata[3].append(maxdim)
+				ydata[4].append(area)
 
+validPoints = []
+for i in xrange(len(xdata[0])):
+	valid = True
+	for yd in ydata:
+		if yd[i] > 1:
+			valid = False
+	if valid:
+		validPoints.append(i)
+
+xdata = [ [ xd[i] for i in validPoints ] for xd in xdata ]
+ydata = [ [ yd[i] for i in validPoints ] for yd in ydata ]
+
+montecarlo = 1000000
 
 if plot == "data":
-	plt.scatter(realError, estError1, c=(0,0,1,0.5), lw=0)
-	plt.plot([0, max(estError1)], [0, max(estError1)], 'r-')
-	# plt.scatter(realError, estError2, c=(1,0,0,0.5), lw=0)
-	# plt.scatter(realError, estError3, c=(0,1,0,0.5), lw=0)
-	n = len(realError)
-	N = 1000000
-	s = 0.
-	for i in xrange(N):
-		a = randrange(n)
-		b = randrange(n)
-		if (realError[a] < realError[b]) == (estError1[a] < estError1[b]):
-			s += 1
-	print s/N
+	ydata = [ ydata[0], ydata[3], ydata[4] ]
+	fig, ax = plt.subplots(len(ydata), len(xdata))
+	for i, xd in enumerate(xdata):
+		for j, yd in enumerate(ydata):
+			ax[j,i].scatter(xd, yd, c=(i/2.,j/2.,1-i/2.,0.5), lw=0)
+			# 0,0: blue
+			# 0,1: cyan
+			# 1,0: red
+			# 1,1: yellow
+			s = 0.
+			for k in xrange(montecarlo):
+				a = randrange(len(xd))
+				b = randrange(len(xd))
+				if (xd[a] > xd[b]) == (yd[a] > yd[b]):
+					s += 1
+			print i, j, s/montecarlo
 
 plt.show()
