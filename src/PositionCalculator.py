23aba1aa2218660d15546273c27ddf7c35becc71
Antoine Maréchal <marechal.an@gmail.com>
2017-09-01 13:19:57 +0100

    Added a TDOA calculation implementation

--- src/PositionCalculator.py
+++ src/PositionCalculator.py
@@ -67,13 +67,13 @@ class PositionCalculator:
 			else:
 				for i in xrange(N):
 					if comp[i] is not None:
-						compiledData.append(comp[i])
+						compiledData[i].append(comp[i])
 		
 		if 0 in [ len(l) for l in compiledData ]:
 			return "incomplete data", np.zeros(3)
 		
 		# average the compiled data
-		compiledData = [ sum(l) / len(l) for l in compiledData ]
+		compiledData = np.array([ sum(l) / len(l) for l in compiledData ])
 		
 		# make the calculation
 		return self.calculate(compiledData)
@@ -86,12 +86,81 @@ class PositionCalculator:
 		"""
 		return []
 	
-	def calculate(self, input):
+	def calculate(self, data):
 		"""Calculates a position estimate from compiled data
 		Must be implemented by the child classes
-		input       -- compiled data
+		data        -- compiled data
 		Returns an error message ("ok" if successful) and the estimated position as a numpy array
 		"""
 		return "not implemented", np.zeros(3)
 	
+
+class TDOACalculator(PositionCalculator):
+	"""Position calculation based on time difference of arrival
+	"""
+	def __init__(self):
+		"""Creates a new calculator with empty data set
+		"""
+		PositionCalculator.__init__(self)
+		self.anchorMin = 4
+		self.anchorMax = 4
 	
+	def compile(self, dataSeries):
+		distDiff = [ None for i in xrange(3) ]
+		if self.anchors[0] in dataSeries:
+			t0, dt0 = dataSeries[self.anchors[0]]
+			for i in xrange(3):
+				if self.anchors[i+1] in dataSeries:
+					t, dt = dataSeries[self.anchors[i+1]]
+					distDiff[i] = (t0 - dt0 - t + dt) * SND_SPEED
+		return distDiff
+	
+	def calculate(self, data):
+		"""Calculates a position estimate from compiled data
+		Must be implemented by the child classes
+		data        -- compiled data
+		Returns an error message ("ok" if successful) and the estimated position as a numpy array
+		"""
+		K = data
+		P = [ self.positions[a] for a in self.anchors ]
+		# solving linear equations
+		M = 2 * (P[0] - np.stack((P[1], P[2], P[3])))
+		I = -2 * K
+		J = K*K + P[0].dot(P[0]) - np.array([ P[i].dot(P[i]) for i in xrange(1,4) ])
+		try:
+			A = np.linalg.inv(M).dot(I)
+			B = np.linalg.inv(M).dot(J)
+		except np.LinAlgErr:
+			return "could not be solved", np.zeros(3)
+		# solving quadratic equation
+		alpha = A.dot(A) - 1
+		beta = 2*A.dot(B) - 2*A.dot(P[0])
+		gamma = B.dot(B) - 2*B.dot(P[0]) + P[0].dot(P[0])
+		delta = beta*beta - 4*alpha*gamma
+		# calculating root(s)
+		if delta < 0:
+			roots = []
+		if delta == 0:
+			roots = [ -beta / (2*alpha) ]
+		if delta > 0:
+			roots = [ (-beta - sqrt(delta)) / (2*alpha),
+			          (-beta + sqrt(delta)) / (2*alpha) ]
+		# selecting valid results
+		positions = []
+		for r in roots:
+			# eliminate negative distances
+			if r < 0:
+				continue
+			# calculate the positon
+			pos = A*r + B
+			# check it's at a valid distance from each anchor
+			if max([ np.linalg.norm(pos-a) for a in P ]) <= SIM_RANGE:
+				positions.append(pos)
+		# return the result
+		if len(positions) == 0:
+			return "no result", np.zeros(3)
+		elif len(positions) == 1:
+			return "ok", positions[0]
+		else:
+			return "multiple results", np.zeros(3)
+		
